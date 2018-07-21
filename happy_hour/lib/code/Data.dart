a209b2f620709906dc5197db041c0d612e2cd170
Josh High <joshhigh99@gmail.com>
2018-07-21 18:39:17 -0500

    added Data.dart and models

---
+++ happy_hour/lib/code/Data.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:core';
+import 'package:happy_hour/models/bar.dart';
+
+class Data{
+  static List<Bar> Bars = List<Bar>();
+
+  static void LoadBarsAndRestaurants() async{
+    
+  }
+}
